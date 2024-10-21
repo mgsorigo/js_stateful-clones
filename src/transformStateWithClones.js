b560e0aecc03a7496527fbbd63d71e424af4808d
mgucma <marek.gucma@gmail.com>
2024-10-21 04:49:52 +0200

    Solution fix 1

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,35 +1,40 @@
 'use strict';
 
 /**
- * @param {Object} state
- * @param {Object[]} actions
+ * @param {Object} state - The initial state object.
+ * @param {Object[]} actions - The actions to be applied on the state.
  *
- * @return {Object[]}
+ * @return {Object[]} - An array of cloned states after each action.
  */
 function transformStateWithClones(state, actions) {
   const array = [];
-  const newState = { ...state };
+  const stateCopy = { ...state };
 
   for (const { type, ...rest } of actions) {
-    if (type === 'addProperties') {
-      Object.assign(newState, rest.extraData);
+    switch (type) {
+      case 'addProperties':
+        Object.assign(stateCopy, rest.extraData);
+        break;
+
+      case 'removeProperties':
+        for (const key of rest.keysToRemove) {
+          delete stateCopy[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key in stateCopy) {
+          delete stateCopy[key];
+        }
+        break;
+
+      default:
+        throw new Error(`Unknown action type: ${type}`);
     }
 
-    if (type === 'removeProperties') {
-      for (const key of rest.keysToRemove) {
-        delete newState[key];
-      }
-    }
+    const clonedState = { ...stateCopy };
 
-    if (type === 'clear') {
-      for (const key in newState) {
-        delete newState[key];
-      }
-    }
-
-    const newState1 = { ...newState };
-
-    array.push(newState1);
+    array.push(clonedState);
   }
 
   return array;
